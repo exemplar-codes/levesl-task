26b5b566b8014132c93873b8939d0c7fe725db23
Muhammad Sanjar Afaq <sanjarcode@gmail.com>
2024-08-24 00:24:23 +0530

    Safe area view android

--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,16 @@
 import React from "react";
-import { SafeAreaView } from "react-native";
+import { SafeAreaView, StatusBar } from "react-native";
 import CountrySearch from "@/screens/CountrySearch";
 
 export default function Index() {
   return (
-    <SafeAreaView style={{ flex: 1, backgroundColor: "white" }}>
+    <SafeAreaView
+      style={{
+        flex: 1,
+        backgroundColor: "white",
+        marginTop: StatusBar.currentHeight,
+      }}
+    >
       <CountrySearch />
     </SafeAreaView>
   );
