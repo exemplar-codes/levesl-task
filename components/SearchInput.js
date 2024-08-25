bd0c5af0aa10779444a5ab68884dcb53f00a12c9
Muhammad Sanjar Afaq <sanjarcode@gmail.com>
2024-08-25 09:53:03 +0530

    Add clear button

--- components/SearchInput.js
+++ components/SearchInput.js
@@ -1,19 +1,16 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-import { View, Image, TextInput, StyleSheet } from "react-native";
+import { View, TextInput, StyleSheet, TouchableOpacity } from "react-native";
+import Icon from "react-native-vector-icons/MaterialIcons";
 import { Colors } from "@/constants/Colors";
 
 export default function SearchInput({ searchText, onChangeText }) {
   return (
     <View style={styles.container}>
-      {/* <Image
-        source={require("../assets/images/Search.svg")}
-        width={20}
-        height={20}
-        alt="searchIcon"
-        onError={console.log}
-      /> */}
+      <View tyle={styles.searchIcon}>
+        <Icon name="search" size={20} />
+      </View>
       <TextInput
         value={searchText}
         onChangeText={onChangeText}
@@ -21,6 +18,14 @@ export default function SearchInput({ searchText, onChangeText }) {
         placeholder="Search"
         selectionColor={Colors.blue.text}
       />
+      {searchText ? (
+        <TouchableOpacity
+          style={styles.clearButton}
+          onPress={() => onChangeText("")}
+        >
+          <Icon name="clear" size={20} />
+        </TouchableOpacity>
+      ) : null}
     </View>
   );
 }
@@ -30,14 +35,21 @@ SearchInput.propTypes = {
 };
 const styles = StyleSheet.create({
   container: {
+    padding: 8,
     flexDirection: "row",
     borderWidth: 1,
     borderColor: "lightgray",
     borderRadius: 8,
+    alignItems: "center",
   },
   input: {
-    padding: 8,
+    marginLeft: 8,
     fontSize: 14,
     fontWeight: "500",
+  },
+  clearButton: {
+    backgroundColor: "lightgray",
+    borderRadius: 32,
+    marginLeft: "auto",
   },
 });
